dc33c117400d00308e923850b818d5e301040e8a
joaquin <jecorrea@protonmail.com>
2022-04-10 18:01:52 -0400

    added validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction < ApplicationRecord
+    #Validations before entering the database
+    validates :payer, :points, :timestamp, presence: true
+    validates :points, numericality: { only_integer: true}
+    validates :points, numericality: { greater_than_or_equal_to: 0}
+
+    
 end
