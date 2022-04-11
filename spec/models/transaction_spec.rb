1ce5b49c377f8075cc44b919b9d06dccc961a01d
joaquin <jecorrea@protonmail.com>
2022-04-10 22:07:53 -0400

    Wrote the unit tests for the model, routes & requests

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has an existing username' do
+    expect(Transaction.create(payer: "eric", points: 7000, timestamp: "2020-11-02T14:00:00Z")).to be_valid
+    expect(Transaction.create(payer: nil, points: 7000, timestamp: "2021-12-02T14:00:00Z")).to_not be_valid
+    expect(Transaction.create(points: 7000, timestamp: "2022-01-02T14:00:00Z")).to_not be_valid
+  end
+
+  it 'has an existing timestamp' do
+    expect(Transaction.create(payer: "eric", points: 7000, timestamp: "2020-11-02T14:00:00Z")).to be_valid
+    expect(Transaction.create(payer: "cindy", points: 7000, timestamp: nil)).to_not be_valid
+    expect(Transaction.create(payer: "jason", points: 7000)).to_not be_valid
+  end
+
+  it 'has existing points' do
+    expect(Transaction.create(payer: "eric", points: 7000, timestamp: "2020-11-02T14:00:00Z").points).to be_a(Integer)
+    expect(Transaction.create(payer: "eric", points: 7000, timestamp: "2020-11-02T14:00:00Z")).to be_valid
+    expect(Transaction.create(payer: "cindy", points: nil, timestamp: "2021-12-02T14:00:00Z")).to_not be_valid
+    expect(Transaction.create(payer: "jason", timestamp: "2022-01-02T14:00:00Z")).to_not be_valid
+  end
+
+  it 'has positive points' do
+    expect(Transaction.create(payer: "eric", points: 7000, timestamp: "2020-11-02T14:00:00Z")).to be_valid
+    expect(Transaction.create(payer: "dannon", points: -200, timestamp: "2021-12-02T14:00:00Z")).to_not be_valid
+  end
 end
--- spec/routing/transactions_routing_spec.rb
+++ spec/routing/transactions_routing_spec.rb
@@ -1,30 +1,17 @@
 require "rails_helper"
 
 RSpec.describe TransactionsController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/transactions").to route_to("transactions#index")
+    describe "routing" do
+      it "routes to #current_balances" do
+      expect(get: "/balances").to route_to("transactions#current_balances")
     end
 
-    it "routes to #show" do
-      expect(get: "/transactions/1").to route_to("transactions#show", id: "1")
+    it "routes to #create_transaction" do
+      expect(post: "/add_transaction").to route_to("transactions#create_transaction")
     end
 
-
-    it "routes to #create" do
-      expect(post: "/transactions").to route_to("transactions#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/transactions/1").to route_to("transactions#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/transactions/1").to route_to("transactions#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/transactions/1").to route_to("transactions#destroy", id: "1")
+    it "routes to #spend_pts" do
+      expect(post: "/spend_points").to route_to("transactions#spend_pts")
     end
   end
 end
